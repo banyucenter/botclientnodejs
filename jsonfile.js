dcbf223abfb849c8c3fdb2d60f2441c8ff05f7b7
Ipung Purwono <32335074+banyucenter@users.noreply.github.com>
2019-02-23 13:29:08 +0700

    Create jsonfile.js

---
+++ jsonfile.js
@@ -0,0 +1,5 @@
+POST /knowledgebases/7b6b8b21-b1a8-4d1d-bee5-6176309488c5/generateAnswer
+Host: https://qnanodebotapp.azurewebsites.net/qnamaker
+Authorization: EndpointKey 7739336f-f037-4a10-8513-994031e04a28
+Content-Type: application/json
+{"question":"<Your question>"}
